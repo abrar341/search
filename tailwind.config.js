756fa678e5450b1949fd2dedc7a5ee0e543c17ae
Your Name <your.email@example.com>
2025-01-21 02:46:01 +0500

    fsfsd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this to match your project's structure
+    "./public/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
